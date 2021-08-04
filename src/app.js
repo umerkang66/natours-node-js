4931c405964341c035a49e2fd9fd343c9c548e43
umerkang66 <umerkang66@gmail.com>
2021-08-04 13:55:26 +0500

    added a little bit of pug templates

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,6 +15,13 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serving static html files
+// app.use(express.static(`${__dirname}/../public`));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // GLOBAL MIDDLEWARES
 // Set security http headers
@@ -52,9 +60,6 @@ app.use(
   })
 );
 
-// Serving static html files
-app.use(express.static(`${__dirname}/../public`));
-
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -63,6 +68,15 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
+// Website Routes
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Northern Lights',
+    user: 'Jonas',
+  });
+});
+
+// Api Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
